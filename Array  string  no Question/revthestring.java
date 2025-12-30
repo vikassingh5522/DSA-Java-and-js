55e77d4d1c979d8f7eff64005e2116302844c41d
vikassingh5522 <vikas.singh66551@gmail.com>
2025-12-30 18:16:29 +0530

    add string reversal implementation in revthestring.java

--- Array  string  no Question/revthestring.java
+++ Array  string  no Question/revthestring.java
@@ -1,3 +1,38 @@
+
+
 public class revthestring {
+
+    public static void main(String[] args) {
+        
+        //using ch
+        // char[] ch = {'h', 'e', 'l', 'l', 'o'};
+        
+        // using string 
+         String str = " programmer!";
+         char ch[] = str.toCharArray();
+        
+
+        int start = 0;
+        int end = ch.length-1;
+        char temp;
+
+        while (start < end) {
+            temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+
+            start ++;
+            end --;
+
+        }
+
+        // for (char c : ch) {
+        //     System.out.print(c + " ");
+        // }
+        
+
+         String reversed = new String(ch);
+        System.out.println(reversed);
+    }
     
 }
