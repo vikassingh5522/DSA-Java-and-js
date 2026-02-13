790751a18df3488507e1a8adc2abee4963d013dc
vikassingh5522 <vikas.singh66551@gmail.com>
2026-02-13 19:14:23 +0530

    add arrr.java implementation to demonstrate array initialization and data types in Java

---
+++ Data Structure Algorithms with Java/Array & String Algorithms/arrr.java
@@ -0,0 +1,30 @@
+
+
+public class arrr {
+    public static void main(String[] args) {
+        
+        // first way to reting arrays
+        int arr[] = { 36, 19, 29, 12, 5, 8, 10, 30, 40, 50, 60, 70, 58, 885, 999, 455 };
+
+        // second way to reting arrays
+          int [] arr2 = new int[10];
+            arr2[0] = 10;
+            arr2[1] = 20;
+            arr2[2] = 30;
+            arr2[3] = 40;
+
+
+            // 8 types of data types in java
+           String [] names = {"deepak", "rahul", "ramesh", "bhuppi", "praveen"};  
+           Float [] marks = { 85.5f, 90.0f, 78.5f, 92.0f, 88.5f };
+           byte [] bytes = { 10, 20, 30, 40, 50 };
+           char [] chars = { 'a', 'b', 'c', 'd', 'e' };
+            boolean [] bools = { true, false, true, false, true };
+            long [] longs = { 10000000000L, 20000000000L, 30000000000L, 40000000000L, 50000000000L };
+            double [] doubles = { 3.14, 2.718, 1.618, 0.577, 1.414 };
+           short [] shorts = { 100, 200, 300, 400, 500 };
+
+
+    }
+    
+}
