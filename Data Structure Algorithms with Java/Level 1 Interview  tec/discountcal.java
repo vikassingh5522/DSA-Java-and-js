88e69e5723d77a9412857dcfad4ab86e8568d695
vikassingh5522 <vikas.singh66551@gmail.com>
2026-03-23 20:32:20 +0530

    add  the tcs

--- Data Structure Algorithms with Java/Level 1 Interview  tec/discountcal.java
+++ Data Structure Algorithms with Java/Level 1 Interview  tec/discountcal.java
@@ -1,4 +1,9 @@
 
+
+
+
+//  this is tcs  questiion 
+
 // given a purchees amount , apply a discound base  on lab and print the final payable amount rounded to 2 decimal places. 
 
 
@@ -11,16 +16,16 @@
 
 // final mount =  Amount - (Amount  * discount rate )
 
-// simple if-else - check the slab boundaries in correct : <1000 first < 5000then  =>5000 else .
+// simple if-else - check the slab boundaries in correct : <1000 first < 5000then  =>5000 else
 
 import java.util.Scanner;
 
-public class discountCal {
+public class discountCal { 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         double amount ;
-        amount =sc.nextDouble();
+        amount = sc.nextDouble();
         double discount;
 
         if(amount<1000){
@@ -32,16 +37,11 @@ public class discountCal {
         else {
             discount= 15.0; //15.0%
         }
-
         double finalAmount = amount * (1 - discount);
 
         System.out.printf("%.2f", finalAmount);
 
         sc.close();
-        
 
-
-    }
-
-    
+    }    
 }
