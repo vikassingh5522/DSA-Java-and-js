f4bbfe53da3323d1e0ac08a59180415c14db2f77
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-07 12:32:56 +0530

    simple to short the array methord

---
+++ Data Structure Algorithms with Java/Level 1 Interview  tec/SortingAlg.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class SortingAlg {
+
+    public static void main(String[] args){
+        
+        int []arr = {1,2,3,5,7};
+        
+        Arrays.sort(arr);
+    
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
