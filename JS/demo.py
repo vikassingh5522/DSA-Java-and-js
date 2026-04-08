808f2a23ac4b6173eecf36d8ac69f1adccf4001c
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-08 20:47:32 +0530

    python book

--- JS/demo.py
+++ JS/demo.py
@@ -1,4 +1,8 @@
 
 
-name = input("enter the name ");
-print(name);
+def add_no(num1 , num2):
+    result = num1 + num2
+    return result
+
+sum_result = add_no(10,5)
+print("the sum is :", sum_result)
