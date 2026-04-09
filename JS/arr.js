aebd9bcf0373ee5036f5b33249d1580b67e50e7a
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-09 23:05:49 +0530

    undefined js

--- JS/arr.js
+++ JS/arr.js
@@ -1,22 +1,22 @@
 
 
-// console.log(1 + "2" + 3);
-// 123
+console.log(1 + "2" + 3);
+123
 
-// let a;
-// console.log(a == undefined, a===undefined);
-// true true 
+let a;
+console.log(a == undefined, a===undefined);
+true true 
 
-// function test(){
-//   console.log(x);
-//   var x =5;
-// }
-// test();
-// undefined
+function test(){
+  console.log(x);
+  var x =5;
+}
+test();
+undefined
 
-// confirm.log([1,2] == [1,2]);  flase --> refrence not same
+confirm.log([1,2] == [1,2]);  flase --> refrence not same
 
-// for (var i = 0; i < 3; i++) {
-//   setTimeout(() => console.log(i), 1000);
-// }
-//  3 3 3 
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+ 3 3 3 
--- JS/demo.py
+++
@@ -1,8 +0,0 @@
-
-
-def add_no(num1 , num2):
-    result = num1 + num2
-    return result
-
-sum_result = add_no(10,5)
-print("the sum is :", sum_result)
