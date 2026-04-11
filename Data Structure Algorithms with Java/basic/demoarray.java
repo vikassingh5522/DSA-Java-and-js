2fc5c785362fe245e035ac6d441bec49cf7cc311
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-11 09:33:07 +0530

    array rev

---
+++ Data Structure Algorithms with Java/basic/demoarray.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+public class demoarray {
+    public static void main(String[] args) {
+
+        // input way of array
+
+        /*
+          
+          int arr[] = {1,2,3,4,5};
+          for(int i = 0 ; i < arr.length ;i++){
+          System.out.print(arr[i]);
+          }
+         
+         */
+
+        /*
+          Scanner sc = new Scanner(System.in);
+          int arr[] = new int[5];
+          System.out.println("enter the arr element  : " );
+          for(int i = 0 ; i < arr.length ; i++){
+          arr[i]= sc.nextInt();
+          }
+          
+          for(int b :arr){
+          System.out.println(b);
+          }
+          
+         */
+
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * System.out.println("enter the arr size :");
+         * int size = sc.nextInt();
+         * 
+         * int arr[] = new int[size];
+         * for(int i = 0 ; i < arr.length ; i++){
+         * System.out.println(" enter the element : " +i);
+         * arr[i] = sc.nextInt();
+         * }
+         * 
+         * 
+         * for(int a: arr){
+         * System.out.println(a);
+         * }
+         * sc.close();
+         * 
+         */
+
+    }
+}
