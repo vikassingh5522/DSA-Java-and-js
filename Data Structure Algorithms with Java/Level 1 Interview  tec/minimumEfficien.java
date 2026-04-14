333efa367e927452b7c382a3e539c63d0947c147
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-15 00:37:06 +0530

    tcs

--- Data Structure Algorithms with Java/Level 1 Interview  tec/minimumEfficien.java
+++ Data Structure Algorithms with Java/Level 1 Interview  tec/minimumEfficien.java
@@ -9,24 +9,50 @@ public class minimumEfficien {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int T =sc.nextInt();
-
+        int T = sc.nextInt();
+        
         int arr[] = new int[1000];
 
-        for(int i = 0 ; i <N ; i++){
-                arr[i] =sc.nextInt();
+        for(int i = 0 ; i < N; i++){
+            arr[i] = sc.nextInt();
         }
         
         Arrays.sort(arr, 0 , N);
 
+        // int maxDiff = 0;
+        //  for (int i = 0; i + T - 1 < N; i += T) {
+        //     int diff = arr[i + T - 1] - arr[i];
+        //     maxDiff = Math.max(maxDiff, diff);
+        // }
+ Arrays.sort(arr, 0, N);
+
         int maxDiff = 0;
-         for (int i = 0; i + T - 1 < N; i += T) {
+
+        int i = 0;  // start index
+
+        // Group processing using while loop
+        while (i < N) {
+
+            // check if full group exists
+            if (i + T - 1 >= N) {
+                break;
+            }
+
             int diff = arr[i + T - 1] - arr[i];
-            maxDiff = Math.max(maxDiff, diff);
+
+            if (diff > maxDiff) {
+                maxDiff = diff;
+            }
+
+            i = i + T; // move to next group
         }
 
-      
+        // Output
         System.out.println(maxDiff);
+
+
+      
+    
 
         sc.close();
 
