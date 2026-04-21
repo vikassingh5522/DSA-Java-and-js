b1eef917f7607fd952ac4e10143aa2f155d83709
vikassingh5522 <vikas.singh66551@gmail.com>
2026-04-21 14:02:47 +0530

    SearchRotatedSortedArray

---
+++ Data Structure Algorithms with Java/letcode/binaryser.java
@@ -0,0 +1,30 @@
+public class binaryser {
+
+    public static void main(String[] args) {
+
+        int arr[] = {-1,2,3,4,5,6,7,8,9,10};
+        int target = 10;
+
+        int li = 0;
+        int ed = arr.length - 1;
+
+        while (li <= ed) {
+
+            int mid = (li + ed) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Found: " + target);
+                return;   // exit program
+            }
+            else if (arr[mid] < target) {
+                li = mid + 1;
+            }
+            else if (arr[mid] > target) {
+                ed = mid - 1;
+            }
+        }
+
+        // if not found
+        System.out.println("Element not found");
+    }
+}
